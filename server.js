d67ddf7a01efe1c42a0484b34ddcb36f90c24a07
Ramish <agamatimtim@gmail.com>
2021-11-22 18:06:37 +0200

    plantSchema fixed

--- server.js
+++ server.js
@@ -2,40 +2,48 @@ const port = process.env.PORT || 3000;
 const express = require('express'),
     bodyParser = require("body-parser"),
     app = express();
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+const {plantSchema, mongooseDB} = require("./mongoose/PlantSchema")
+
+let db = mongooseDB.connection
+//
 
 
-let itemStore = [`start`, 12];
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("connected");
 
-app.get(['/',`/item`], function (req, res) {
-    res.json(itemStore);
+
+    let itemStore = [`start`, 12];
+
+    app.get(['/', `/item`], function (req, res) {
+        res.json(itemStore);
+    });
+
+
+    app.get('/item/:id', function (req, res) {
+        res.json(itemStore[req.params.id]);
+    });
+
+
+    app.post('/item', function (req, res) {
+        itemStore.push(req.body);
+        res.json(req.body);
+    });
+
+    app.put('/item/:id', function (req, res) {
+        itemStore[req.params.id] = req.body
+        res.json(req.body);
+    });
+
+
+    app.delete('/item/:id', function (req, res) {
+        itemStore.splice(req.params.id, 1)
+        res.json(req.body);
+    });
+
+
+    app.listen(port, () => console.log(`live on ${port}`))
+
 });
-
-
-
-app.get('/item/:id', function (req, res) {
-    res.json(itemStore[req.params.id]);
-});
-
-
-app.post('/item', function (req, res) {
-    itemStore.push(req.body);
-    res.json(req.body);
-});
-
-app.put('/item/:id', function (req, res) {
-    itemStore[req.params.id] = req.body
-    res.json(req.body);
-});
-
-
-app.delete('/item/:id', function (req, res) {
-    itemStore.splice(req.params.id, 1)
-    res.json(req.body);
-});
-
-
-app.listen(port, () => console.log(`live on ${port}`))
