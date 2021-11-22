204eb99688fff3a237bf1ae8205c614b9f7a45cc
Ramish <agamatimtim@gmail.com>
2021-11-22 18:10:15 +0200

    changed all end points to async

--- server.js
+++ server.js
@@ -17,28 +17,28 @@ db.once('open', function () {
 
     let itemStore = [`start`, 12];
 
-    app.get(['/', `/item`], function (req, res) {
+    app.get(['/', `/item`], async function (req, res) {
         res.json(itemStore);
     });
 
 
-    app.get('/item/:id', function (req, res) {
+    app.get('/item/:id', async function (req, res) {
         res.json(itemStore[req.params.id]);
     });
 
 
-    app.post('/item', function (req, res) {
+    app.post('/item', async function (req, res) {
         itemStore.push(req.body);
         res.json(req.body);
     });
 
-    app.put('/item/:id', function (req, res) {
+    app.put('/item/:id', async function (req, res) {
         itemStore[req.params.id] = req.body
         res.json(req.body);
     });
 
 
-    app.delete('/item/:id', function (req, res) {
+    app.delete('/item/:id', async function (req, res) {
         itemStore.splice(req.params.id, 1)
         res.json(req.body);
     });
