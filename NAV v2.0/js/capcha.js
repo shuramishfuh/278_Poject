853d36c52d40dee5f2dedbe347c0fafcab934e38
Ibrahim Mohammed <ism07@aub.edu.lb>
2021-12-05 03:28:28 +0200

    new updates

---
+++ NAV v2.0/js/capcha.js
@@ -0,0 +1,15 @@
+
+function get_action(form) 
+{
+    var v = grecaptcha.getResponse();
+    if(v.length == 0)
+    {
+        document.getElementById('captcha').innerHTML="You can't leave Captcha Code empty";
+        return false;
+    }
+    else
+    {
+         document.getElementById('captcha').innerHTML="Captcha completed";
+        return true; 
+    }
+}
